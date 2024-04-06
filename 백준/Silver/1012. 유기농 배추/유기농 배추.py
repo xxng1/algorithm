9bde604a13e6f895018111a7e9e7c2103d867dae
박상웅 <woongaa1@naver.com>
2024-04-06 23:40:39 +0900

    [Silver II] Title: 유기농 배추, Time: 320 ms, Memory: 34096 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,47 +1,39 @@
 from collections import deque
 
-# cnt = 0
-def bfs(x, y):
-    # global cnt
+dx = [0,0,1,-1]
+dy = [1,-1,0,0]
+
+t = int(input())
+
+def bfs(graph, a, b):
     queue = deque()
-    # 상 하 좌 우  | (1,0) -> (0, 0) = 상, (1,0) -> (1, 1) 우
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
-    
-    queue.append((x, y))
-    while(queue):
+    queue.append((a,b))
+    graph[a][b] = 0
+
+    while queue:
         x, y = queue.popleft()
-
         for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
+            nx = x+dx[i]
+            ny = y+dy[i]
+            if nx < 0 or nx >=n or ny < 0 or ny >= m:
+                continue
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = 0
+                queue.append((nx, ny))
+    return
 
-            if 0 <= nx < M and 0<= ny < N:
-                if graph[nx][ny] == 1:
-                    queue.append((nx,ny))
-                    graph[nx][ny] = 0
-                    # for i in range(4):
-                    #     nx = x + dx[i]
-                    #     ny = y + dy[i]
-
-                    #     if 0 <= nx < M and 0<= ny < N:
-                    #         test[ny][nx] = 0
-                            
-
-T = int(input())
-
-for i in range(T):
-    M, N, K = map(int, input().split())
-    graph = [[0]* N for _ in range(M)]
-    for i in range(K):
-        a, b = map(int, input().split())
-        graph[a][b] = 1
-
+for i in range(t):
     cnt = 0
-    for a in range(M):
-        for b in range(N):
+    n, m, k = map(int,input().split())
+    graph = [[0]*m for _ in range(n)]
+
+    for j in range(k):
+        x, y = map(int, input().split())
+        graph[x][y] = 1
+
+    for a in range(n):
+        for b in range(m):
             if graph[a][b] == 1:
-                bfs(a,b)
+                bfs(graph, a, b)
                 cnt += 1
-    
     print(cnt)
