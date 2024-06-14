e5a5a74a5c47b77e6dfb62f37f74a37e6b41f82c
박상웅 <woongaa1@naver.com>
2024-06-14 18:22:46 +0900

    [level 2] Title: 과제 진행하기, Time: 1.47 ms, Memory: 10.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.py
@@ -0,0 +1,38 @@
+def solution(plans):
+    answer = []
+    stack = []
+
+    for i in range(len(plans)):
+        a, b = map(int, plans[i][1].split(':'))
+        plans[i][1] = a * 60 + b
+        plans[i][2] = int(plans[i][2])
+
+    plans.sort(key = lambda x : x[1])
+
+    for i in range(len(plans) - 1):
+        stack.append(plans[i])
+        gap = plans[i+1][1] - plans[i][1]
+
+        #print(gap)
+        while stack and gap:
+            if stack[-1][2] <= gap:
+                a, b, c = stack.pop()
+                gap -= c
+                answer.append(a)
+            else:
+                stack[-1][2] -= gap
+                gap = 0
+
+    answer.append(plans[-1][0])
+
+    for i in range(len(stack)):
+        answer.append(stack[~i][0])
+            
+
+
+
+    
+
+
+    
+    return answer
