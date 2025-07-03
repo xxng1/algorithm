2bdcde8b7da1fbaf393cc37490e3195238768e0e
박상웅 <woongaa1@naver.com>
2025-07-03 09:00:59 +0900

    [Silver I] Title: 단지번호붙이기, Time: 36 ms, Memory: 32488 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,33 +1,49 @@
 N = int(input())
-graph = [list(map(int, input())) for _ in range(N)]
+graph = []
+
+for i in range(N):
+    graph.append(list((input())))
+
 visited = [[0] * N for _ in range(N)]
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
+count = 0
+
 def dfs(x, y):
     global count
-    visited[x][y] = 1
-    count += 1
+    visited[x][y] = True
+
+    if graph[x][y] == '0':
+        return
 
     for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if nx < 0 or nx >= N or ny < 0 or ny >= N:
-            continue
-        if graph[nx][ny] == 1 and visited[nx][ny] == 0:
-            dfs(nx, ny)
+        nx = dx[i] + x
+        ny = dy[i] + y
 
-count = 0
-result = []
+        if 0 <= nx < N and 0 <= ny < N:
+            if visited[nx][ny] == False:
+                if graph[nx][ny] == '1':
+                    # visited[nx][ny] = True
+                    count += 1
+                    dfs(nx, ny)
+    
+
+
+answer = []
+cnt = 0
 
 for i in range(N):
     for j in range(N):
-        if graph[i][j] == 1 and visited[i][j] == 0:
-            count = 0
-            dfs(i, j)
-            result.append(count)
+        if graph[i][j] == '1' and visited[i][j] == 0:
+            count = 1
+            dfs(i, j) 
+            if count != 0:
+                cnt += 1
+                answer.append(count)
 
-print(len(result))
-for r in sorted(result):
-    print(r)
+answer.sort()
+print(cnt)
+for i in answer:
+    print(i)
