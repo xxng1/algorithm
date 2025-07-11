772dfcae7b704ab4bd08694894a860fc0b7bd1d0
박상웅 <woongaa1@naver.com>
2025-07-11 15:59:38 +0900

    [Silver II] Title: 유기농 배추, Time: 64 ms, Memory: 34992 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,39 +1,77 @@
 from collections import deque
+import sys
 
-dx = [0,0,1,-1]
-dy = [1,-1,0,0]
+input = sys.stdin.readline
 
-t = int(input())
+T = int(input())
 
-def bfs(graph, a, b):
+
+# visited = [[0] * M for _ in range(N)]
+
+
+
+
+
+def bfs(x, y):
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+    visited[y][x] = 1
+
     queue = deque()
-    queue.append((a,b))
-    graph[a][b] = 0
-
+    queue.append((y, x))
+    
     while queue:
-        x, y = queue.popleft()
+        y, x = queue.popleft()
+
         for i in range(4):
-            nx = x+dx[i]
-            ny = y+dy[i]
-            if nx < 0 or nx >=n or ny < 0 or ny >= m:
-                continue
-            if graph[nx][ny] == 1:
-                graph[nx][ny] = 0
-                queue.append((nx, ny))
-    return
+            nx = dx[i] + x
+            ny = dy[i] + y
 
-for i in range(t):
+            if ( 0 <= nx < M and 0 <= ny < N):
+                if ( visited[ny][nx] == 0 and graph[ny][nx] == 1 ):
+                    visited[ny][nx] = 1
+                    queue.append((ny, nx))
+
+# print(graph[1][1])
+# for i in graph:
+#     print(i)
+
+# print('----')
+
+
+# for i in visited:
+#     print(i)
+
+# print('----')
+# bfs(0,0)
+
+# for i in visited:
+#     print(i)
+
+
+for TestCase in range(T):
     cnt = 0
-    n, m, k = map(int,input().split())
-    graph = [[0]*m for _ in range(n)]
+    
 
-    for j in range(k):
-        x, y = map(int, input().split())
-        graph[x][y] = 1
+    M, N, K = map(int, input().split())
+    visited = [[0] * M for _ in range(N)]
 
-    for a in range(n):
-        for b in range(m):
-            if graph[a][b] == 1:
-                bfs(graph, a, b)
+    graph = [[0] * M for _ in range(N)]
+
+
+    for _ in range(K):
+        a, b = map(int, input().split())
+    
+        graph[b][a] = 1
+
+
+    for i in range(N):
+        for j in range(M):
+            if graph[i][j] == 1 and visited[i][j] == 0:
+                bfs(j, i)
                 cnt += 1
+
+    # for i in visited:
+    #     print(i)
+
     print(cnt)
