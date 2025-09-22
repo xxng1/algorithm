f13625948a2eef21f09bf3d26f651fc839802adb
박상웅 <woongaa1@naver.com>
2025-09-22 19:44:44 +0900

    [level 3] Title: 등대, Time: 4987.13 ms, Memory: 43.5 MB -BaekjoonHub

--- 프로그래머스/3/133500. 등대/등대.py
+++ 프로그래머스/3/133500. 등대/등대.py
@@ -1,36 +1,36 @@
-import sys
-sys.setrecursionlimit(10**6)
-
+from collections import defaultdict,deque
 def solution(n, lighthouse):
-    if n <= 3:
-        return 1
-    
-    answer = 0
-    
-    visited = [False] * (n+1)
-    graph = [[] for _ in range(n+1)]
-    dp = [[0, 0] for _ in range(n+1)]
-    
+    graph = defaultdict(list)
+    onoff = [0 for _ in range(n + 1)]
+
     for a, b in lighthouse:
         graph[a].append(b)
         graph[b].append(a)
+
+    q = deque()
+    # 리프 노드 담기
+    for i in range(1, n+1):
+        if len(graph[i]) == 1:
+            q.append(i)
+
+    # 리프 노드부터 루트까지 올라가기, 등대 켜지면 다음 노드와 연결 끊기
+    while q:
+        now_leaf = q.popleft()
+        # if graph[now_leaf] == []:
+        if len(graph[now_leaf]) == 0:
+            break
+        parent = graph[now_leaf][0]
+
+        # 리프 노드 그래프에서 삭제
+        del graph[now_leaf]
+        # 부모 노드에서 리프 노드 연결 해제
+        graph[parent].remove(now_leaf)
+        # 부모 노드가 리프 노드가 되면 큐에 넣기
+        if len(graph[parent]) == 1:
+            q.append(parent)
+
+        if onoff[now_leaf] == 1:
+            continue
+        onoff[parent] = 1
     
-    
-    def dfs(start):
-        visited[start] = True
-        dp[start][0] = 0
-        dp[start][1] = 1
-        
-        
-        for i in graph[start]:
-            if not visited[i]:
-                dfs(i)
-                dp[start][0] += dp[i][1]
-                dp[start][1] += min(dp[i][0], dp[i][1])
-    
-    
-    dfs(1)
-    
-    # print(dp)
-    
-    return min(dp[1][0], dp[1][1])
+    return sum(onoff)
