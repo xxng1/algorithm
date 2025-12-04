adb3bb8062fb662f66c8ed96e34c21d6e81a198d
박상웅 <woongaa1@naver.com>
2025-12-04 22:46:18 +0900

    [Gold IV] Title: 점수따먹기, Time: 920 ms, Memory: 34456 KB -BaekjoonHub

--- 백준/Gold/1749. 점수따먹기/점수따먹기.py
+++ 백준/Gold/1749. 점수따먹기/점수따먹기.py
@@ -1,57 +1,28 @@
-# arr= [
-#     [2, 3, -21, -22, -23],
-#     [5, 6, -22, -23, -25],
-#     [-22, -23, 4, 10, 2]
-# ]
-
-# N, M = 3, 5
-
 import sys
 input = sys.stdin.readline
 
-N, M = map(int, input().split())
+n, m = map(int, input().split())
 arr = []
-for _ in range(N):
-    arr.append(list(map(int, input().split())))
+for _ in range(n):
+    row = list(map(int, input().split()))
+    arr.append(row)
 
-prefix = [[0] * (M + 1) for _ in range(N+1)]
+def get_max_sum(row):
+    cur, best = row[0], row[0]
+    for x in row[1:]:
+        cur = max(x, cur + x)
+        best = max(cur, best)
+    return best
 
-# for i in prefix:
-#     print(i)
+ans = -1e9
+for start in range(n):
+    temp = [0] * m
+    for i in range(start, n):
+        row = arr[i]
+        for j in range(m):
+            temp[j] += row[j]
+        cur_max = get_max_sum(temp)
+        ans = max(ans, cur_max)
+print(ans)
 
-
-for i in range(1, N+1):
-    for j in range(1, M+1):
-        prefix[i][j] = ( arr[i-1][j-1]
-                        + prefix[i-1][j]
-                        + prefix[i][j-1]
-                        - prefix[i-1][j-1])
-
-# for i in prefix:
-#     print(i)
-
-# x1, y1 = 1, 0
-# x2, y2 = 2, 4
-
-# S = ( prefix[x2+1][y2+1]
-#      - prefix[x1][y2+1]
-#      - prefix[x2+1][y1]
-#      + prefix[x1][y1])
-
-# print(S)
-
-
-answer = -1e9
-
-for x1 in range(N):
-    for x2 in range(x1, N):
-        for y1 in range(M):
-            for y2 in range(y1, M):
-                # print(x1, x2, y1, y2)
-                S = ( prefix[x2+1][y2+1]
-                        - prefix[x1][y2+1]
-                        - prefix[x2+1][y1]
-                        + prefix[x1][y1])
-                if S > answer:
-                    answer = S
-print(answer)
+# print(temp)
